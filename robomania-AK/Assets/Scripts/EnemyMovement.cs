d8a33773f9b83a2abc5e6931b9d39fe244296372
BillySwCodeNinjas <130806226+MinnieMia@users.noreply.github.com>
2023-05-24 18:52:57 -0500

    finished

--- robomania-AK/Assets/Scripts/EnemyMovement.cs
+++ robomania-AK/Assets/Scripts/EnemyMovement.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     public float yForce;
+    public float xForce;
+    public float xDirection;
     private Rigidbody2D enemyRigidBody;
-    void start(){
+    void Awake(){
         enemyRigidBody = GetComponent<Rigidbody2D>();
     }
     private void FixedUpdate(){
         if(transform.position.x <= -8){
-            
+            xDirection = 1;
+            enemyRigidBody.AddForce(Vector2.right*xForce);
         }
         if(transform.position.x >= 8){
-            
+            xDirection = -1;
+            enemyRigidBody.AddForce(Vector2.left*xForce);
         }
         
     }
    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
-        {
-            Vector2 jumpForce = new Vector2(0, yForce);
-            enemyRigidBody.AddForce(jumpForce); // adding a force
-        }
+      if(collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Enemy"){
+        Vector2 jumpForce = new Vector2(xForce*xDirection,yForce);
+        enemyRigidBody.AddForce(jumpForce);
+      }
+      if(collision.gameObject.tag == "Player"){
+        Vector2 bounceBack = new Vector2(xForce * xDirection * 3, yForce * -4);
+        enemyRigidBody.AddForce(bounceBack);
+      }
     }
 }
